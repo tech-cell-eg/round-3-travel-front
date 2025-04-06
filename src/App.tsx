7a7bb46bb3dedb9ea8b4d0900871b8b3efac4b31
Mohamed Magdy <93835752+moamed018@users.noreply.github.com>
2025-04-06 23:02:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
-import { Checkbox, CheckboxChangeEvent } from 'primereact/checkbox';
+import { Checkbox } from 'primereact/checkbox';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [checked, setChecked] = useState(false);
 
   return (
