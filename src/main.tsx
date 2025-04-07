2702e570d92416e052ee735f13343498a02d735d
Hodayousry2752 <hanaayoussrey@gmail.com>
2025-04-07 19:48:53 +0200

    added routing and navbar folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import 'primereact/resources/themes/lara-light-blue/theme.css'; 
+import ReactDom from "react-dom/client";
+import App from './App'
+import 'primereact/resources/themes/lara-light-indigo/theme.css';  
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const queryClient: QueryClient = new QueryClient();
-createRoot(document.getElementById('root')!).render(
-  
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <App />
-    </QueryClientProvider>
-    
-  </StrictMode>,
+
+let projectRoot = ReactDom.createRoot(document.getElementById('root')!);
+projectRoot.render(
+<App/>
 )
