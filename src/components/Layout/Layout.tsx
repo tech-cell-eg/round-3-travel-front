1efddad34c31e3950ade60e89b2dad21d225d217
olaalam <cds.olaallam382330@alexu.edu.eg>
2025-04-08 00:20:17 +0200

    created footer items

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,11 +6,12 @@ export default function Layout() {
   return (
     <>
     <Navbar/>
-     <div className=' py-10 w-full'>
-      
-      <Outlet></Outlet>
-     </div>
-    <Footer/>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Outlet/>
+      </div>
+      <Footer/>
+    </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import App from './App'
 import 'primereact/resources/themes/lara-light-indigo/theme.css';  
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import"./index.css"
 
 let projectRoot = ReactDom.createRoot(document.getElementById('root')!);
 projectRoot.render(
 <App/>
+
 )
