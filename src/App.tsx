83a5552c8d930bd07cf803a7f2cfd446a5520fb5
Hodayousry2752 <hanaayoussrey@gmail.com>
2025-04-10 15:00:18 +0200

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
-import About from './components/About/About';
 
 function App() {
   const router = createBrowserRouter(
@@ -12,7 +11,6 @@ function App() {
         element: <Layout />,
         children: [
           { index: true, element: <Home /> },
-          { path: 'about', element: <About /> },
         ],
       },
     ],
