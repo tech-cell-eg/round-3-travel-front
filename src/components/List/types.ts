ef62261a47f883a6952753537cb2f2bc8e81691d
Hodayousry2752 <hanaayoussrey@gmail.com>
2025-04-13 16:50:38 +0200

    complited list com and bugs

---
+++ src/components/List/types.ts
@@ -0,0 +1,12 @@
+// types.ts
+
+export type Tour = {
+    id: string;
+    name: string;
+  };
+  
+  export type Language = {
+    id: string;
+    languages: string; // تأكد إنها "languages" لو جاية من الـ API كده
+  };
+  
